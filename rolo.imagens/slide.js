cd03b7ce6c14d36b169040844d496d0dc7d67f2b
jonathan souza <jonathanlobosc89@gmail.com>
2025-06-19 01:28:37 -0300

    alteração texto

--- rolo.imagens/slide.js
+++ rolo.imagens/slide.js
@@ -1,18 +1,19 @@
-var radio = document.querySelector('.manual-btn')
-var cont = 1
+let cont = 1; // Controla o slide atual (começa no 1)
 
-document.getElementById('radio1').checked = true /* para pintar a bola do passador*/
+document.getElementById('radio1').checked = true; 
+// Ativa o primeiro slide automaticamente ao carregar a página
 
 setInterval(() => {
-    proximaImg()        /*tempo em que vai pssar as imagens*/
-}, 5000)
+    proximaImg(); // Função chamada a cada 5 segundos
+}, 5000); // Tempo de troca dos slides (5000 ms = 5 segundos)
 
-function proximaImg(){
-    cont++
+function proximaImg() {
+    cont++; // Avança para o próximo slide
 
-    if(cont > 3){
-        cont = 1        /* para que volte para a preimera bolinha do passador*/
+    if (cont > 3) {
+        cont = 1; // Reinicia para o primeiro slide após o último
     }
 
-    document.getElementById('radio'+cont).checked = true
+    document.getElementById('radio' + cont).checked = true;
+    // Marca o botão de rádio correspondente ao slide atual
 }
