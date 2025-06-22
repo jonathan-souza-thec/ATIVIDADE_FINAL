83ed4e2fa84ad8db823b0e1cfd9a6579a3a4657d
jonathan souza <jonathanlobosc89@gmail.com>
2025-06-22 15:04:23 -0300

    ultima alteração

--- rolo.imagens/slide.js
+++ rolo.imagens/slide.js
@@ -1,19 +1,17 @@
-let cont = 1; // Controla o slide atual (começa no 1)
+let cont = 1; 
 
 document.getElementById('radio1').checked = true; 
-// Ativa o primeiro slide automaticamente ao carregar a página
 
 setInterval(() => {
-    proximaImg(); // Função chamada a cada 5 segundos
-}, 5000); // Tempo de troca dos slides (5000 ms = 5 segundos)
+    proximaImg(); 
+}, 5000); 
 
 function proximaImg() {
-    cont++; // Avança para o próximo slide
+    cont++; 
 
     if (cont > 3) {
-        cont = 1; // Reinicia para o primeiro slide após o último
+        cont = 1; 
     }
 
     document.getElementById('radio' + cont).checked = true;
-    // Marca o botão de rádio correspondente ao slide atual
 }
